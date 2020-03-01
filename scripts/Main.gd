4746fc66305b510c865e7b14ebf23fe0743271ab
Animo <omrisim@gmail.com>
2020-03-01 20:58:23 +0200

    Huge scalability refactor
    Way too tired to list the changes, also missing features from previous demo that
    will be implemented some other time.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,19 +2,19 @@ extends Node2D
 
 # TODO: organize this file, it's a bit fugly
 
-var Enemy = preload("res://scenes/BaseEnemy.tscn")
-var player_moved_once = false
-onready var hero = $Hero
-onready var hud = $HUD
-onready var move_marker = $MoveMarker
+var Enemy = preload("res://units/enemies/ranged/FirstEnemy/FirstEnemy.tscn")
+var player_moved_once: bool = false
+onready var hero: Node = $Hero
+onready var hud: Node = $HUD
+onready var move_marker: Node = $MoveMarker
 
 func _ready() -> void:
 	hud.start(
-		hero.attack_cooldown.wait_time,
-		hero.attack_cooldown.time_left
+		hero._attack_cooldown.wait_time,
+		hero._attack_cooldown.time_left
 	)
 
-func _on_Hero_moved(target_pos: Vector2) -> void:
+func _on_Hero_move_target_updated(target_pos: Vector2) -> void:
 	move_marker.position = target_pos
 	if not player_moved_once:
 		move_marker.show()
--- scripts/MeleeHero.gd
+++
@@ -1,9 +0,0 @@
-extends "res://scripts/BaseHero.gd"
-
-# TODO: implement heroes with melee auto attacks
-
-func _ready() -> void:
-	pass
-
-func attack() -> void:
-	pass
